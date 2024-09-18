db414a4dc4c2ae1b534a845ebecdd357b1d16b11
Jenish Kothari <kotharijenish2001@gmail.com>
2024-09-18 16:45:54 -0400

    Update in the routes of App.tsx

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -19,7 +19,6 @@ export default function Kanbas() {
               <Route path="/Courses/:cid/*" element={<Courses />} />
               <Route path="/Calendar" element={<h1>Calendar</h1>} />
               <Route path="/Inbox" element={<h1>Inbox</h1>} />
-
             </Routes>
           </td>
         </tr>
