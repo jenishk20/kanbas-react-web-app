2f2e190312617333fd2e7ce84baec442d174cb01
Jenish Kothari <kotharijenish2001@gmail.com>
2024-09-19 12:47:36 -0400

    Adding up more courses and submodules.

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -20,6 +20,24 @@ export default function Modules() {
                 </li>
               </ul>
             </li>
+            <li className="wd-lesson">
+              <span className="wd-title">READING</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Understanding the HTML Layout
+                </li>
+                <li className="wd-content-item">
+                  Understanding the Canvas Structure
+                </li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">HTML Tags </li>
+                <li className="wd-content-item">React Routing</li>
+              </ul>
+            </li>
           </ul>
         </li>
         <li className="wd-module">
@@ -34,6 +52,34 @@ export default function Modules() {
                 </li>
               </ul>
             </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Introduction to HTML Elements and Attributes
+                </li>
+                <li className="wd-content-item">
+                  Structuring a Webpage Using HTML
+                </li>
+                <li className="wd-content-item">
+                  Best Practices for Semantic HTML
+                </li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">READING</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  HTML & CSS: Design and Build Websites - Chapter 1: Structure
+                  of an HTML Document
+                </li>
+                <li className="wd-content-item">MDN Web Docs - HTML Basics</li>
+                <li className="wd-content-item">
+                  Full Stack Developer Guide - Chapter 3: Building a Webpage
+                  Layout with HTML
+                </li>
+              </ul>
+            </li>
           </ul>
         </li>
 
@@ -45,6 +91,27 @@ export default function Modules() {
               <ul className="wd-content">
                 <li className="wd-content-item">Introduction to CSS</li>
                 <li className="wd-content-item">Styling your webpage</li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">READING</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Full Stack Developer - Chapter 1
+                </li>
+                <li className="wd-content-item">
+                  Full Stack Developer - Chapter 2
+                </li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Introduction to Wev Development
+                </li>
+                <li className="wd-content-item">Creating an HTTP Server</li>
+                <li className="wd-content-item">Creating an Git Account</li>
               </ul>
             </li>
           </ul>
