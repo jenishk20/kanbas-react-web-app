7f96cf8a9a2ba27323fd08a8c7a15d93b8b69521
Jenish Kothari <kotharijenish2001@gmail.com>
2024-10-03 18:17:56 -0400

    Landing Page Update

--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -1,28 +1,37 @@
-import { Link } from "react-router-dom";
-
+import React from "react";
+import { Link } from "react-router-dom"; // Make sure to import Link
+import "./styles.css";
 export default function Landing() {
   return (
-    <div>
+    <div className="container text-center mt-5">
       <h1>Jenish Sunil Kothari : Section 1</h1>
-      <h2>Lab Assignments</h2>
-      <Link to="/Labs">Go to Labs</Link>
-
-      <h2>Kanbas Application</h2>
-      <Link to="/Kanbas">Go to Kanbas</Link>
-
-      <h2>Source Code Repos</h2>
-      <ul>
-        <li>
-          <a
-            href="https://github.com/jenishk20/kanbas-react-web-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Kanbas Source Code
-          </a>
-        </li>
-        
-      </ul>
+      <div className="sub-component">
+        <h2>Lab Assignments</h2>
+        <Link to="/Labs" className="btn btn-primary mb-3">
+          Go to Labs
+        </Link>
+      </div>
+      <div className="sub-component">
+        <h2>Kanbas Application</h2>
+        <Link to="/Kanbas" className="btn btn-primary mb-3">
+          Go to Kanbas
+        </Link>
+      </div>
+      <div className="sub-component">
+        <h2>Source Code Repos</h2>
+        <ul className="list-unstyled">
+          <li>
+            <a
+              href="https://github.com/jenishk20/kanbas-react-web-app"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-link"
+            >
+              Kanbas Source Code
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
